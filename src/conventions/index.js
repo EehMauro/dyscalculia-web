83c1f5e60b33d75284ffed96d77a046a8182b430
EehMauro <maurojoaquinescudero@gmail.com>
2017-11-29 15:20:36 -0300

    Switch to test environment

--- src/conventions/index.js
+++ src/conventions/index.js
@@ -1,8 +1,7 @@
 import colors from './colors';
 import getTheme from './getTheme';
 
-//const API_URL = 'http://52.205.212.82:8001';
-const API_URL = 'http://192.168.0.46:8080';
+const API_URL = 'http://52.205.212.82:8003';
 
 export {
   colors,
