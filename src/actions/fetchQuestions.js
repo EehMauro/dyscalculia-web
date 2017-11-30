6ddfbbcf4df42ed167d5c2884e5b55175bb6b015
EehMauro <maurojoaquinescudero@gmail.com>
2017-11-30 20:19:03 -0300

    Bulk commit

--- src/actions/fetchQuestions.js
+++ src/actions/fetchQuestions.js
@@ -1,5 +1,20 @@
 import { API_URL } from '../conventions';
 
+function preloadImages (questions) {
+  let images = [];
+  let preloaded = [];
+  for (let question of questions) {
+    if (question.image) images.push(question.image);
+    if (question.type === 'image-multiple-choice-question') {
+      images.push(...question.options);
+    }
+  }
+  for (let i = 0; i < images.length; i++) {
+    preloaded[i] = new Image();
+    preloaded[i].src = images[i];
+  }
+}
+
 export default function () {
 
   return async (dispatch, getState) => {
@@ -13,6 +28,8 @@ export default function () {
       let { results: questions } = response;
 
       if (!questions) throw response;
+
+      preloadImages(questions);
 
       dispatch({ type: 'FETCH_QUESTIONS_SUCCESS', questions });
 
